9c1f84dfd61344ff4d2f35a596139b0972309288
Anoia <github@stuckinadrawer.com>
2022-04-06 22:20:03 +0200

    init

---
+++ src/views/CookBook.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <header class="flex bg-slate-500 items-center">
+            <h1 class="px-10 my-7 font-serif text-3xl text-white grow drop-shadow-md">Cookbook</h1>
+            <p class="px-10 text-white grow-0 right-1">Browse | Search | Create</p>
+        </header>
+
+        <div class="container mx-auto max-w-4xl">
+            <div class="flex flex-row">
+                <img class="m-5 max-w-[200px]" src="https://via.placeholder.com/200" />
+                <div class="flex flex-col m-5">
+                    <h2
+                        class="my-10 text-3xl font-bold text-slate-800 grow"
+                    >Kürbissuppe mit Ingwer und Kokosmilch</h2>
+                    <p class="relative bottom-0 grow-0 text-slate-500 text-right">written by Anoia</p>
+                </div>
+            </div>
+
+            <div class="flex flex-col sm:flex-row">
+                <div class="basis-1/3 m-5 border-slate-400">
+                    <h3 class="text-xl my-2 px-2 py-1 bg-slate-300">Ingredients</h3>
+                    <ul class="my-3 space-y-1">
+                        <li class="border-b-[1px] p-1 border-slate-300">600g Kürbis</li>
+                        <li class="border-b-[1px] p-1 border-slate-300">300g Karotte</li>
+                        <li class="p-1">200ml Kokosmilch</li>
+                    </ul>
+                </div>
+                <div class="basis-2/3 m-5">
+                    <h3 class="text-xl my-2 px-2 py-1 bg-slate-300">Directions</h3>
+                    <div class="flex my-5">
+                        <span class="text-3xl p-1 pr-4 text-slate-500">1</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class="flex my-5">
+                        <span class="text-3xl p-1 pr-4 text-slate-500">2</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="flex min-h-full flex-col items-center justify-center">
+    <img alt="Vue logo" src="../assets/logo.png" class="mt-10" />
+    <button
+      @click="count++"
+      class="rounded-md border bg-emerald-600 py-1 px-3 text-sm font-medium text-white hover:bg-emerald-500"
+    >Count: {{ count }}</button>
+    <HelloWorld msg="Vite + Vue 3 + Typescript + Tailwind 3 + Vue Rounter 4" />
+    <div class="mt-10 inline-flex rounded-md shadow">
+      <router-link
+        to="/about"
+        class="inline-flex items-center justify-center rounded-md border border-transparent bg-slate-600 px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-slate-500 focus:outline-none"
+      >Next Page</router-link>
+      <router-link
+        to="/cookbook"
+        class="inline-flex items-center justify-center rounded-md border border-transparent bg-slate-600 px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-slate-500 focus:outline-none"
+      >CookBook</router-link>
+    </div>
+  </div>
+</template>
